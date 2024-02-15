2a002fad2dd9e58594119548c289fa5d206d5963
Simon <simoonf@vip.qq.com>
2024-02-15 10:06:39 +0800

    feat: add austand template

---
+++ templates/template-react-zustand/src/pages/welcome/welcome.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+
+import logo from "@/assets/logo.svg";
+import { Header } from "@/components/header/header";
+
+import styles from "./welcome.module.less";
+
+export const Welcome: FC = () => {
+  const navigate = useNavigate();
+
+  const onContinue = () => navigate("/demo");
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <Header>🚀 Vite + React Boilerplate</Header>
+        <p>
+          <button onClick={onContinue}>Continue</button>
+        </p>
+        <p>
+          <a
+            className={styles.appLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className={styles.appLink}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+};
---
+++ templates/template-react-zustand/src/services/types/api.types.ts
@@ -0,0 +1,5 @@
+export type GetUserResult = {
+  id: number;
+  name: string;
+  username: string;
+};
